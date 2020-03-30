2d623d783c51f30e0f33d79a29eec0f7912b3c23
AdrienPit <48018636+AdrienPit@users.noreply.github.com>
2020-03-30 10:37:50 +0200

    MAJ

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,37 +1,18 @@
-const app = document.getElementById('root')
+/*!
+    * Start Bootstrap - SB Admin v6.0.0 (https://startbootstrap.com/templates/sb-admin)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+    (function($) {
+    "use strict";
 
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
+    // Add active state to sidbar nav links
+    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+        $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+            if (this.href === path) {
+                $(this).addClass("active");
+            }
+        });
 
-app.appendChild(container)
-
-var request = new XMLHttpRequest()
-var url = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/The%20Xytoz'
-var key = 'api_key='+'RGAPI-f23e7642-dde7-4ce6-9ff9-e8ef1c751f26'
-request.open('GET', url+'?'+key, true)
-request.onload = function() {
-   // Begin accessing JSON data here
-   var data = JSON.parse(this.response)
-  
-   if (true) {
-     
-     
-       
- 
-     const p = document.createElement('p')
-     p.textContent = data.accountId
-     var accountId = data.accountId
- 
-    
- 
-     container.appendChild(p)
-       
-    
-   } else {
-     const errorMessage = document.createElement('h1')
-     errorMessage.textContent = `Gah, it's not working!`
-     app.appendChild(errorMessage)
-   }
-}
-
-request.send()
+   
+})(jQuery);
